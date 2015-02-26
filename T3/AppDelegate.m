b6766afafadbe5d2939e13952bb6665bbd32cfda
HazemB <hazem.beshara@live.ca>
2015-02-26 15:18:42 -0500

    firstpatch_1

--- T3/AppDelegate.m
+++ T3/AppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by Developer on 15-02-26.
 //  Copyright (c) 2015 hazem_test. All rights reserved.
 //
-//TO TEST COMMIT WITHOUT PUSH
+//TO TEST COMMIT WITHOUT PUSHdsfsdaf
 
 #import "AppDelegate.h"
 
